0e56fb12187879379b289de6dbe01df126b071fc
vagner ventura <vagnerventura@hotmail.com>
2025-05-27 10:57:17 -0300

    Cria entidade Address e relaciona com User e Order

---
+++ src/main/java/com/LaMusic/entity/Address.java
@@ -0,0 +1,65 @@
+package com.LaMusic.entity;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.LaMusic.util.Auditable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Getter
+@Setter
+@Entity
+@Table(name = "addresses")
+public class Address extends Auditable {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String number;
+
+    private String complement;
+
+    @CreationTimestamp
+    @Column(name = "created_at", columnDefinition = "TIMESTAMPTZ")
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMPTZ")
+    private OffsetDateTime updatedAt;
+    
+}
