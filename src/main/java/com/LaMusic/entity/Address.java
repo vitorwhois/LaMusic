3bc4aa6c8316096b31ae14a77f2a2fe78e40a933
vagner ventura <vagnerventura@hotmail.com>
2025-05-31 19:28:19 -0300

    refactor: adiciona relacionamento ManyToOne com User

--- src/main/java/com/LaMusic/entity/Address.java
+++ src/main/java/com/LaMusic/entity/Address.java
@@ -3,9 +3,6 @@ package com.LaMusic.entity;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import com.LaMusic.util.Auditable;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -17,56 +14,45 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
-
-@Getter
-@Setter
 @Entity
 @Table(name = "addresses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Address extends Auditable {
-
     @Id
     @GeneratedValue
     private UUID id;
-
-    @Column(name = "user_id", nullable = false)
-    private UUID userId;
-
-    @Column(nullable = false)
-    private String cep;
-
-    @Column(nullable = false)
-    private String country;
-
-    @Column(nullable = false)
-    private String state;
-
-    @Column(nullable = false)
-    private String city;
-
-    @Column(nullable = false)
-    private String street;
-
-    @Column(nullable = false)
-    private String number;
-
-    private String complement;
-
-    @CreationTimestamp
-    @Column(name = "created_at", columnDefinition = "TIMESTAMPTZ")
-    private OffsetDateTime createdAt;
-
-    @UpdateTimestamp
-    @Column(name = "updated_at", columnDefinition = "TIMESTAMPTZ")
-    private OffsetDateTime updatedAt;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     @JsonBackReference
     private User user;
+
+    private String type;
+    private String label;
+    private String recipientName;
+    private String street;
+    private String number;
+    private String complement;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+    private boolean isDefault;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMPTZ")
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMPTZ")
+    private OffsetDateTime updatedAt;
 }
