ae74bdac53cd71eb35a55a5af28d369d171d1ac1
vagner ventura <vagnerventura@hotmail.com>
2025-05-31 19:28:36 -0300

    refactor: adiciona relacionamento ManyToMany com Product via tabela intermediária

--- src/main/java/com/LaMusic/entity/Category.java
+++ src/main/java/com/LaMusic/entity/Category.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.UUID;
 
 import com.LaMusic.util.Auditable;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,6 +32,19 @@ public class Category extends Auditable{
 	
 	private String name;
 	
+	private String slug;
+	
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "parent_id")
+	private Category parent;
+	
+	private Integer sortOrder;
+	
+	private boolean isActive;
+	
+	@JsonIgnore
 	@ManyToMany(mappedBy = "categories")
 	private List<Product> products;
 	
