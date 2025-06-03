5f4e990847d07c89d2c5eb742e5b7d5f06775800
vagner ventura <vagnerventura@hotmail.com>
2025-06-03 11:01:39 -0300

    Refatora AuthController para registrar clientes e admins, e gerar JWT

--- src/main/java/com/LaMusic/controllers/AuthController.java
+++ src/main/java/com/LaMusic/controllers/AuthController.java
@@ -1,46 +1,76 @@
 package com.LaMusic.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.LaMusic.dto.*;
+import com.LaMusic.dto.AuthRequest;
+import com.LaMusic.dto.AuthResponse;
+import com.LaMusic.dto.RegisterRequest;
+import com.LaMusic.entity.User;
+import com.LaMusic.enums.Role;
+import com.LaMusic.repositories.UserRepository;
 import com.LaMusic.security.JwtUtil;
+import com.LaMusic.services.UserDetailsServiceImpl;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-	@Autowired
-	private AuthenticationManager authenticationManager;
-	
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
-	@Autowired
-	private JwtUtil jwtUtil;
-	
-	
-	
-	@PostMapping
-	public LoginResponse login (@RequestBody LoginRequest request) {
-	
-		authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(request.email(),
-				request.password()));
-	
-		final UserDetails userDetails = userDetailsService.loadUserByUsername(request.email());	
-		final String jwt = jwtUtil.generateToken(userDetails);	
-		return new LoginResponse(jwt);	
-	
-	}
-	
-	
-	
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return new AuthResponse(jwt);
+    }
+
+    @PostMapping("/register")
+    public AuthResponse registerCliente(@RequestBody RegisterRequest request) {
+        return registerUser(request, Role.USER);
+    }
+
+    @PostMapping("/register-admin")
+    public AuthResponse registerAdmin(@RequestBody RegisterRequest request) {
+        return registerUser(request, Role.ADMIN);
+    }
+
+    private AuthResponse registerUser(RegisterRequest request, Role role) {
+        if (userRepository.findByEmail(request.getEmail()).isPresent()){
+            throw new RuntimeException("E-mail já cadastrado.");
+        }
+
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setPhone(request.getPhone());
+        user.setRole(role);
+        user.setEmail_verified(false);
+
+        userRepository.save(user);
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmail());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return new AuthResponse(jwt);
+    }
 }
